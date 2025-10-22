e35774a3e81f77add832eb8d0840ee72a901fbf8
khoomo21 <119529812+khoomo21@users.noreply.github.com>
2025-10-22 16:38:31 +0500

    Update hw.sql

--- lesson-7/homework/hw.sql
+++ lesson-7/homework/hw.sql
@@ -1,1 +1,33 @@
+/* ============================================================
+   🟢 EASY-LEVEL TASKS (10)
+   ============================================================ */
+
+-- 1) MIN цена продукта (Products)
+SELECT MIN(Price) AS MinPrice
+FROM dbo.Products;
+GO
+
+-- 2) MAX зарплата (Employees)
+SELECT MAX(Salary) AS MaxSalary
+FROM dbo.Employees;
+GO
+
+-- 3) количество строк в Customers
+SELECT COUNT(*) AS CustomerCount
+FROM dbo.Customers;
+GO
+
+-- 4) число уникальных категорий в Products
+SELECT COUNT(DISTINCT Category) AS DistinctCategoryCount
+FROM dbo.Products;
+GO
+
+-- 5) общая сумма продаж по продукту id = 7 (Sales)
+-- Вариант A: если есть Amount (сумма строки)
+SELECT SUM(Amount) AS TotalSales_Product7
+FROM dbo.Sales
+WHERE ProductID = 7;
+
+-- Вариант B: если Amount нет, но есть Quantity и UnitPrice
+-- SELECT SUM(Qua
 
